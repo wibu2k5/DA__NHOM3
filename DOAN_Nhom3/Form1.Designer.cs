487b55d55cc9ca6eb43ee9d373a4aff5b82f3248
nhut <truonghuynhminhnhut@gmail.com>
2025-12-15 19:33:31 +0700

    Add project files.

---
+++ DOAN_Nhom3/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace DOAN_Nhom3
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+   
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+
+        private void InitializeComponent()
+        {
+            this.rtbEditor = new System.Windows.Forms.RichTextBox();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnRedo = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+
+            // Menu và các item
+            menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+
+            // ========== menuStrip1 ==========
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+        this.fileToolStripMenuItem,
+        this.editToolStripMenuItem
+    });
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1447, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+
+            // ========== File ==========
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+        this.saveToolStripMenuItem,
+        new System.Windows.Forms.ToolStripSeparator(),
+        this.exitToolStripMenuItem
+    });
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "&File";
+
+            // Save (Ctrl+S)
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(240, 34);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.BtnSaveClick);
+
+            // Exit (Alt+F4)
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(240, 34);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitMenuClick);
+
+            // ========== Edit ==========
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+        this.undoToolStripMenuItem,
+        this.redoToolStripMenuItem
+    });
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(58, 29);
+            this.editToolStripMenuItem.Text = "&Edit";
+
+            // Undo (Ctrl+Z)
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(240, 34);
+            this.undoToolStripMenuItem.Text = "&Undo";
+            this.undoToolStripMenuItem.Click += new System.EventHandler(this.BtnUndoClick);
+
+            // Redo (Ctrl+Y)
+            this.redoToolStripMenuItem.Name = "redoToolStripMenuItem";
+            this.redoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Y)));
+            this.redoToolStripMenuItem.Size = new System.Drawing.Size(240, 34);
+            this.redoToolStripMenuItem.Text = "&Redo";
+            this.redoToolStripMenuItem.Click += new System.EventHandler(this.BtnRedoClick);
+
+            // ========== panel1 (thanh nút) ==========
+            this.panel1.Controls.Add(this.btnUndo);
+            this.panel1.Controls.Add(this.btnRedo);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 33); // dưới menu
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1447, 127);
+            this.panel1.TabIndex = 1;
+            // (Nếu bạn có panel1_Paint và muốn giữ, thêm lại sau. Không bắt buộc.)
+
+            // ========== btnUndo ==========
+            this.btnUndo.Location = new System.Drawing.Point(114, 40);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(385, 60);
+            this.btnUndo.TabIndex = 1;
+            this.btnUndo.Text = "Undo (Ctrl+Z)";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.BtnUndoClick);
+
+            // ========== btnRedo ==========
+            this.btnRedo.Location = new System.Drawing.Point(572, 40);
+            this.btnRedo.Name = "btnRedo";
+            this.btnRedo.Size = new System.Drawing.Size(385, 60);
+            this.btnRedo.TabIndex = 2;
+            this.btnRedo.Text = "Redo (Ctrl+Y)";
+            this.btnRedo.UseVisualStyleBackColor = true;
+            this.btnRedo.Click += new System.EventHandler(this.BtnRedoClick);
+
+            // ========== btnSave ==========
+            this.btnSave.Location = new System.Drawing.Point(1025, 40);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(385, 60);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save (Ctrl+S)";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSaveClick);
+
+            // ========== rtbEditor ==========
+            this.rtbEditor.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.rtbEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbEditor.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.rtbEditor.Location = new System.Drawing.Point(0, 160);
+            this.rtbEditor.Name = "rtbEditor";
+            this.rtbEditor.Size = new System.Drawing.Size(1447, 682);
+            this.rtbEditor.TabIndex = 0;
+            this.rtbEditor.Text = "";
+            this.rtbEditor.TextChanged += new System.EventHandler(this.RtbEditorTextChanged);
+
+            // ========== Form1 ==========
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1447, 842);
+
+            // Thứ tự Add: RichTextBox (Fill) trước, panel (Top) sau, menu (Top) cuối để menu nằm trên cùng
+            this.Controls.Add(this.rtbEditor);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+
+            this.panel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+        #endregion
+
+        private RichTextBox rtbEditor;
+        private Button btnUndo;
+        private Button btnRedo;
+        private Button btnSave;
+        private Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redoToolStripMenuItem;
+    }
+}
---
+++ DOAN_Nhom3/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;            
+using System.Runtime.InteropServices; // 
+
+namespace DOAN_Nhom3
+{
+    public partial class Form1 : Form
+    {
+        // Lịch sử Undo/Redo
+        private readonly Stack<string> undoStack = new();
+        private readonly Stack<string> redoStack = new();
+        private bool isInternalChange = false;     // chặn TextChanged khi set Text bằng code
+        private string previousText = string.Empty;
+        private string? currentFilePath = null;    // đường dẫn file đã lưu
+
+        // Placeholder “Nhập tại đây…”
+        private Label? lblPlaceholder;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object? sender, EventArgs e)
+        {
+            // Trạng thái ban đầu
+            previousText = rtbEditor.Text;
+            UpdateUndoRedoButtons();
+
+            // Chặn Undo/Redo/Save mặc định của RichTextBox, để menu xử lý phím tắt
+            rtbEditor.KeyDown += RtbEditorKeyDown;
+
+            // Đảm bảo gõ được
+            rtbEditor.ReadOnly = false;
+            rtbEditor.Enabled = true;
+
+            // Tạo placeholder
+            var placeholder = new Label
+            {
+                Text = "Nhập tại đây...",
+                ForeColor = Color.Gray,
+                BackColor = rtbEditor.BackColor,
+                AutoSize = true,
+                Cursor = Cursors.IBeam
+            };
+            lblPlaceholder = placeholder;
+
+            // Đặt label chồng lên rtbEditor
+            Control container = rtbEditor.Parent ?? this;
+            container.Controls.Add(placeholder);
+            PositionPlaceholder();
+            placeholder.BringToFront();
+            placeholder.Click += (s, e2) => rtbEditor.Focus();
+            rtbEditor.LocationChanged += (s, e2) => PositionPlaceholder();
+            rtbEditor.SizeChanged += (s, e2) => PositionPlaceholder();
+            this.Resize += (s, e2) => PositionPlaceholder();
+
+            // Cập nhật placeholder theo trạng thái
+            rtbEditor.GotFocus += (s, e2) => UpdatePlaceholder();
+            rtbEditor.LostFocus += (s, e2) => UpdatePlaceholder();
+            UpdatePlaceholder();
+
+            // Ẩn thanh nút vì đã có MenuStrip (nếu muốn gỡ hẳn, xem RemoveButtonsBar ở dưới)
+            RemoveButtonsBar();
+
+            this.Text = "Trình soạn thảo cơ bản - Undo/Redo (Stack)";
+            // Đăng ký file association .nh3 (per-user)
+            EnsureFileAssociation();
+
+            // Nếu app được mở bằng cách double-click file .nh3, nạp nội dung
+            var args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                string candidate = args[1];
+                if (File.Exists(candidate))
+                    LoadFromFile(candidate);
+            }
+        }
+
+        // Nếu muốn gỡ hẳn panel + nút (thay vì chỉ ẩn), gọi hàm này trong Form1_Load thay cho panel1.Visible = false
+        private void RemoveButtonsBar()
+        {
+            if (btnUndo != null && !btnUndo.IsDisposed) panel1.Controls.Remove(btnUndo);
+            if (btnRedo != null && !btnRedo.IsDisposed) panel1.Controls.Remove(btnRedo);
+            if (btnSave != null && !btnSave.IsDisposed) panel1.Controls.Remove(btnSave);
+
+            if (panel1 != null && !panel1.IsDisposed)
+            {
+                this.Controls.Remove(panel1);
+                panel1.Dispose();
+                // Không bắt buộc: panel1 = null!;
+            }
+        }
+
+        // Text thay đổi do người dùng
+        private void RtbEditorTextChanged(object? sender, EventArgs e)
+        {
+            if (isInternalChange)
+            {
+                UpdatePlaceholder();
+                return;
+            }
+
+            undoStack.Push(previousText);
+            redoStack.Clear();
+            previousText = rtbEditor.Text;
+
+            UpdateUndoRedoButtons();
+            UpdatePlaceholder();
+        }
+
+        // Chỉ chặn phím để không chạy Undo/Redo mặc định của RichTextBox.
+        // Hành động sẽ do Menu (ShortcutKeys) gọi qua Click.
+        private void RtbEditorKeyDown(object? sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.Z)
+            {
+                Undo();
+                e.SuppressKeyPress = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.Y)
+            {
+                Redo();
+                e.SuppressKeyPress = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.S)
+            {
+                SaveToFile();
+                e.SuppressKeyPress = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.C)
+            {
+                // Fix: nếu đang Select All thì copy bản đã bỏ xuống dòng cuối
+                if (IsAllSelected(rtbEditor))
+                {
+                    var trimmed = rtbEditor.Text.TrimEnd('\r', '\n');
+                    if (string.IsNullOrEmpty(trimmed))
+                        Clipboard.Clear();        // không copy kí tự xuống dòng “trống”
+                    else
+                        Clipboard.SetText(trimmed);
+
+                    e.SuppressKeyPress = true;    // chặn Copy mặc định để không copy CR/LF cuối
+                    e.Handled = true;
+                }
+                // nếu không phải Select All => để mặc định (KHÔNG suppress)
+            }
+        }
+
+        // Nút và menu đều dùng các handler sau
+        private void BtnUndoClick(object? sender, EventArgs e) => Undo();
+        private void BtnRedoClick(object? sender, EventArgs e) => Redo();
+        private void BtnSaveClick(object? sender, EventArgs e) => SaveToFile();
+        private void ExitMenuClick(object? sender, EventArgs e) => Close();
+
+        private void Undo()
+        {
+            if (undoStack.Count == 0) return;
+
+            isInternalChange = true;
+            redoStack.Push(rtbEditor.Text);
+            rtbEditor.Text = undoStack.Pop();
+            isInternalChange = false;
+
+            previousText = rtbEditor.Text;
+            rtbEditor.SelectionStart = rtbEditor.TextLength;
+            rtbEditor.SelectionLength = 0;
+
+            UpdateUndoRedoButtons();
+            UpdatePlaceholder();
+        }
+
+        private void Redo()
+        {
+            if (redoStack.Count == 0) return;
+
+            isInternalChange = true;
+            undoStack.Push(rtbEditor.Text);
+            rtbEditor.Text = redoStack.Pop();
+            isInternalChange = false;
+
+            previousText = rtbEditor.Text;
+            rtbEditor.SelectionStart = rtbEditor.TextLength;
+            rtbEditor.SelectionLength = 0;
+
+            UpdateUndoRedoButtons();
+            UpdatePlaceholder();
+        }
+
+
+        private void UpdateUndoRedoButtons()
+        {
+            bool canUndo = undoStack.Count > 0;
+            bool canRedo = redoStack.Count > 0;
+
+            // Nếu panel ẩn thì các nút vẫn tồn tại, nhưng ta check cho chắc
+            if (btnUndo != null && !btnUndo.IsDisposed) btnUndo.Enabled = canUndo;
+            if (btnRedo != null && !btnRedo.IsDisposed) btnRedo.Enabled = canRedo;
+
+            // Đồng bộ menu
+            if (undoToolStripMenuItem != null) undoToolStripMenuItem.Enabled = canUndo;
+            if (redoToolStripMenuItem != null) redoToolStripMenuItem.Enabled = canRedo;
+        }
+
+        private void UpdatePlaceholder()
+        {
+            if (lblPlaceholder == null) return;
+            // Hiện khi trống
+            lblPlaceholder.Visible = string.IsNullOrEmpty(rtbEditor.Text);
+        }
+
+        private void PositionPlaceholder()
+        {   
+            if (lblPlaceholder == null) return;
+            lblPlaceholder.Location = new Point(rtbEditor.Left + 6, rtbEditor.Top + 6);
+        }
+        // File association
+        private const string CustomExt = ".nh3";                       // đuôi riêng
+        private const string CustomProgId = "DOAN_Nhom3.Document";     // ProgID cho Windows
+
+        [DllImport("Shell32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern void SHChangeNotify(uint wEventId, uint uFlags, IntPtr dwItem1, IntPtr dwItem2);
+        private const uint SHCNE_ASSOCCHANGED = 0x8000000;
+        private const uint SHCNF_IDLIST = 0x0;
+        // Đăng ký mở .nh3 bằng ứng dụng hiện tại (phạm vi user)
+
+        private static void EnsureFileAssociation()
+        {
+            try
+            {
+                using var classes = Registry.CurrentUser.OpenSubKey(@"Software\Classes", writable: true)
+                                   ?? Registry.CurrentUser.CreateSubKey(@"Software\Classes");
+
+                // 1) .nh3 -> ProgID
+                using (var extKey = classes.OpenSubKey(CustomExt, writable: true) ?? classes.CreateSubKey(CustomExt))
+                {
+                    var cur = extKey.GetValue(null) as string;
+                    if (!string.Equals(cur, CustomProgId, StringComparison.OrdinalIgnoreCase))
+                        extKey.SetValue(null, CustomProgId);
+
+                    using var openWith = extKey.OpenSubKey("OpenWithProgids", writable: true) ?? extKey.CreateSubKey("OpenWithProgids");
+                    openWith.SetValue(CustomProgId, string.Empty, RegistryValueKind.String);
+                }
+
+                // 2) ProgID -> lệnh mở + icon
+                using (var progKey = classes.OpenSubKey(CustomProgId, writable: true) ?? classes.CreateSubKey(CustomProgId))
+                {
+                    progKey.SetValue(null, "Nhom3 Document"); // mô tả
+
+                    using var iconKey = progKey.OpenSubKey("DefaultIcon", writable: true) ?? progKey.CreateSubKey("DefaultIcon");
+                    iconKey.SetValue(null, $"\"{Application.ExecutablePath}\",0");
+
+                    using var cmdKey = progKey.OpenSubKey(@"shell\open\command", writable: true) ?? progKey.CreateSubKey(@"shell\open\command");
+                    cmdKey.SetValue(null, $"\"{Application.ExecutablePath}\" \"%1\"");
+                }
+
+                // 3) Báo cho Explorer biết association đã thay đổi
+                SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, IntPtr.Zero, IntPtr.Zero);
+            }
+            catch
+            {
+                // Bị chặn bởi policy hoặc lỗi registry -> bỏ qua để app vẫn chạy
+            }
+        }
+        private void LoadFromFile(string path)
+        {
+            try
+            {
+                if (!File.Exists(path)) return;
+
+                isInternalChange = true;
+                rtbEditor.Text = File.ReadAllText(path, Encoding.UTF8);
+                isInternalChange = false;
+
+                previousText = rtbEditor.Text;
+                currentFilePath = path;
+                this.Text = "Trình soạn thảo - " + Path.GetFileName(currentFilePath);
+
+                undoStack.Clear();
+                redoStack.Clear();
+                UpdateUndoRedoButtons();
+                UpdatePlaceholder();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Không thể mở file:\n" + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void SaveToFile()
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(currentFilePath))
+                {
+                    File.WriteAllText(currentFilePath!, rtbEditor.Text, Encoding.UTF8);
+                    MessageBox.Show("Đã lưu: " + currentFilePath, "Lưu file",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    using var sfd = new SaveFileDialog
+                    {
+                        Title = "Lưu văn bản",
+                        Filter = "Nhom3 Document (*.nh3)|*.nh3|Text Files (*.txt)|*.txt|All Files (*.*)|*.*",
+                        DefaultExt = "nh3",
+                        AddExtension = true,
+                        FileName = "document.nh3"
+                    };
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        File.WriteAllText(sfd.FileName, rtbEditor.Text, Encoding.UTF8);
+                        currentFilePath = sfd.FileName;
+                        this.Text = "Trình soạn thảo - " + Path.GetFileName(currentFilePath);
+                        MessageBox.Show("Đã lưu: " + currentFilePath, "Lưu file",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Không thể lưu file:\n" + ex.Message, "Lỗi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+            private static bool IsAllSelected(RichTextBox r)
+        {
+            return r.SelectionStart == 0 && r.SelectionLength == r.TextLength;
+        }
+        private void CopySelectedText()
+        {
+            if (IsAllSelected(rtbEditor))
+            {
+                var trimmed = rtbEditor.Text.TrimEnd('\r', '\n');
+                if (string.IsNullOrEmpty(trimmed)) Clipboard.Clear();
+                else Clipboard.SetText(trimmed);
+            }
+            else
+            {
+                rtbEditor.Copy(); // copy mặc định cho lựa chọn thường
+            }
+        }
+
+    }
+}
